a501a2f0d651dc30d413228b63dd359360f80782
YannickSpoerl <spoerlyannick@gmail.com>
2019-11-18 00:30:31 +0100

    Begin implementing joinLecture + http request

--- lib/screens/join_lecture.dart
+++ lib/screens/join_lecture.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/io_client.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import '../widgets/button.dart';
 import '../widgets/inputText.dart';
-import '../widgets/inputTime.dart';
 
 class JoinLecture extends StatefulWidget {
   JoinLecture({Key key}) : super(key: key);
@@ -16,17 +19,48 @@ class _JoinLectureState extends State<JoinLecture> {
   final _formKey = GlobalKey<FormState>();
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController controller;
+  final lecturePinController = TextEditingController();
   var qrText = "LECTURE #12345";
+  var allLectures = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllLectures().then((value) => setLectures(value));
+  }
+
+  setLectures(lectures) {
+    print(lectures);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    lecturePinController.dispose();
+    super.dispose();
+  }
+
+  _getAllLectures() async {
+    var httpClient = new HttpClient()..badCertificateCallback =
+    ((X509Certificate cert, String host, int port) => true);
+    IOClient ioClient = new IOClient(httpClient);
+    var uri = new Uri.https('192.168.2.138:8888', '/lectures');
+    var response = await ioClient.get(uri);
+    var responseBody = response.body;
+    return responseBody;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Join a lecture'),
+      ),
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-         // StyleInputText("Enter Lecture PIN"),
+          StyleInputText("Enter Lecture PIN", lecturePinController),
           Center(child: Text("or scan provided QR-Code")),
           Expanded(
               flex: 5,
@@ -36,7 +70,11 @@ class _JoinLectureState extends State<JoinLecture> {
           Center(
             child: new Container(
                 margin: const EdgeInsets.all(3),
-                child: StyleButton("Join lecture")),
+                child: StyleButton("Join lecture",
+                    onPressed:(){
+                  print(allLectures);
+                  print("Lecture PIN: ${lecturePinController.text}");
+                    })),
           ),
         ],
       ),
